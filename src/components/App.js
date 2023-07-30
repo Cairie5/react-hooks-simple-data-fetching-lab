e4a1796260d72dd1bcb3bd0372883854897bbcd9
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-07-30 21:56:03 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const App = () => {
-  const [dogImage, setDogImage] = useState('');
+function App() {
+  // State to hold the random dog image URL
+  const [dogImage, setDogImage] = useState(null);
+
+  // State to track if the API call is in progress
   const [isLoading, setIsLoading] = useState(true);
 
+  // useEffect hook to fetch data from the API on component mount
   useEffect(() => {
-    // Fetch data from the API
-    fetch('https://dog.ceo/api/breeds/image/random')
+    // Fetch data from the dog.ceo API
+    fetch("https://dog.ceo/api/breeds/image/random")
       .then((response) => response.json())
       .then((data) => {
-        // Update state with the received data
+        // Update the state with the received dog image URL
         setDogImage(data.message);
         setIsLoading(false); // Set loading state to false after receiving data
-      })
-      .catch((error) => {
-        console.error('Error fetching dog image:', error);
-        setIsLoading(false); // Set loading state to false even on error
       });
-  }, []); // Empty dependency array to ensure useEffect runs only once
+  }, []); // Empty dependency array to ensure useEffect runs only once on mount
 
-  return (
-    <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <img src={dogImage} alt="A Random Dog" />
-      )}
-    </div>
-  );
-};
+  // If the API call is in progress, display a "Loading..." message
+  if (isLoading) return <p>Loading...</p>;
+
+  // Once the API call is completed and the dogImage state is set, display the dog image
+  return <img src={dogImage} alt="A Random Dog" />;
+}
 
 export default App;
